e99abf76011c469790a903f3d1b505da3a1d33cd
RolandMarton <roland111187@gmail.com>
2025-07-14 18:50:45 +0200

    Fix the scss variable handling and import mechanism so React can use the :root variables now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
+import "./styles/index.scss";
+
 function App() {
   return (
     <>
-      <h1 style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", width: "100vw" }}>Welcome to the Homework Project!</h1>
+      <h1 style={{color: "var(--color-accent)"}}>Welcome to the Homework Project!</h1>
     </>
   );
 }
