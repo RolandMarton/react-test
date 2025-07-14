6ac7bf4a358c16d46653fb1f95cad305cee9e9a9
RolandMarton <roland111187@gmail.com>
2025-07-14 18:57:33 +0200

    Migrating sass @import to @use as it will be deprecated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import "./styles/index.scss";
 function App() {
   return (
     <>
-      <h1 style={{color: "var(--color-accent)"}}>Welcome to the Homework Project!</h1>
+      <h1>Welcome to the Homework Project!</h1>
     </>
   );
 }
