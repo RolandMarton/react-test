0738bd22f42a2e442f7289973b0bade1030f491e
RolandMarton <roland111187@gmail.com>
2025-07-15 11:28:39 +0200

    Add shoppin counter

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,23 @@
+import { useContext } from "react";
 import { cart } from "../assets";
+import CartContext from "../store/cart-context";
 
 const Header = () => {
+  const cartCtx = useContext(CartContext);
+  const totalItemsCount = cartCtx.items.length;
+
   return (
     <header className="header_container">
       <h6 className="header_title">HALLO WORLD</h6>
       <div className="shopping_cart_container">
         <img src={cart} alt="Shopping cart" />
         <p className="shopping_cart_text">Shopping Cart</p>
+
+        {totalItemsCount > 0 && (
+          <div className="shopping_cart_item_count_container">
+            <p className="shopping_cart_item_count">{totalItemsCount}</p>
+          </div>
+        )}
       </div>
     </header>
   );
